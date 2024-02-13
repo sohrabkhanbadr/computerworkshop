ed275305efa13c23471fa8bb5d3071fa226cfa52
Sohrab Khanbadr <sohrabkhanbadr@gmail.com>
2024-02-13 19:28:22 +0330

    Add files via upload

---
+++ CW_PHP_backend/formHandling/form-handler.php
@@ -0,0 +1,11 @@
+<?php
+
+// getting the value of name field
+$name = $_POST["name"];
+// getting the value of the email field
+$email = $_POST["email"];
+
+echo "Hi, ". $name . "<br>";
+echo "Your email address: ". $email ."<br>";
+
+?>
